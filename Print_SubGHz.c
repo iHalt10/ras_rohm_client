0459b6d755da61a81c83c7f044a3777e75ec906f
池田敬祐 <keisuke0908@ike-keisukeMacBook-Air.local>
2019-08-19 16:45:11 +0900

    kei 5th commit Edit Lazrite Program

--- Print_SubGHz.c
+++ Print_SubGHz.c
@@ -1,32 +1,31 @@
 #include "Print_SubGHz_ide.h"		// Additional Header
 
+//Motor
 #define MotorA1	4
 #define MotorA2 5
 #define MotorB1 6
 #define MotorB2 7
 const uint8_t speeds = 255;
 
-#define SUBGHZ_CH	36
-#define SUBGHZ_PANID	0xABCD
+//Communication
+#define SUBGHZ_CH 36
+#define SUBGHZ_PANID 0xABCD
 uint8_t rx_data[256];
 
-#define BLUE_LED	26
+//LED
+#define BLUE_LED 26
 
 static const unsigned char *aes_key = NULL;		// disable AES key
 
 void setup(void) {
 	SUBGHZ_MSG msg;
-
 	Serial.begin(115200);
-	
 	msg = SubGHz.init();
 	if(msg != SUBGHZ_OK) {
 		SubGHz.msgOut(msg);
 		while(1){ }
 	}
-
 	SubGHz.setKey(aes_key);
-	
 	msg = SubGHz.begin(SUBGHZ_CH, SUBGHZ_PANID,  SUBGHZ_100KBPS, SUBGHZ_PWR_20MW);
 	if(msg != SUBGHZ_OK) {
 		SubGHz.msgOut(msg);
@@ -37,16 +36,14 @@ void setup(void) {
 		SubGHz.msgOut(msg);
 		while(1){ }
 	}
-
 	pinMode(BLUE_LED,OUTPUT);
 	digitalWrite(BLUE_LED,HIGH);
-
 	return;
+
 }
 
 void loop(void) {
-	short rx_len;
-	
+	short rx_len;	
 	rx_len = SubGHz.readData(rx_data,sizeof(rx_data));
 	rx_data[rx_len] = 0;
 	if(rx_len > 0) {
@@ -57,22 +54,26 @@ void loop(void) {
 		
 	}
 	return;
+
 }
 
-
+//Move Motor
 void Motor_Move(void){
 	analogWrite(MotorA1,speeds);
 	analogWrite(MotorA2,0);
 	analogWrite(MotorB1,speeds);
 	analogWrite(MotorB2,0);
   	delay(1000);
+
 }
 
+//Stop Motor 
 void Motor_Stop(void){
 	analogWrite(MotorA1,speeds);
 	analogWrite(MotorA2,speeds);
 	analogWrite(MotorB1,speeds);
 	analogWrite(MotorB2,speeds);
-//	delay(1000);
+
 }
+
 
